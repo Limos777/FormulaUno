ef770fbf1c34b8a647a640c7733fc02dd96394ce
AlessandroLimongi <alessandro.limongi.15@gmail.com>
2025-10-15 16:45:29 +0200

    Created component and routing

--- formulaUno/src/app/app.routes.ts
+++ formulaUno/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Routes } from "@angular/router";
+import { Home } from "./home/home";
+import { GranPremio } from "./gran-premio/gran-premio";
+import { ElencoGranPremio } from "./elenco-gran-premio/elenco-gran-premio";
+ 
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'Home page'
+    },
+    {
+        path: '',
+        component: GranPremio,
+        title: 'Dettagli del gran premio'
+    },
+    {
+        path: '',
+        component: ElencoGranPremio,
+        title: 'Elenco del gran premio'
+    }
+]
